c80df8ca0cfb210b784d54e132a59ef7d9d907b5
Marina Tavares <marinastavares6@gmail.com>
2020-05-15 15:58:26 -0300

    revert

--- sub.js
+++ sub.js
@@ -3,28 +3,9 @@ var mqtt = require('mqtt')
 var client  = mqtt.connect('mqtt://mqtt.eclipse.org/')
 var topic = 'cookies'
 
-require('./src/db/mongoose')
-const MQTTdata = require('./src/models/dataMQTT')
-
 client.on('message', (topic, message) => {
     message = message.toString()
     console.log(message)
-    if (message.includes('{') && !message.includes('mqtt')) {
-        const data = JSON.parse(message)
-        console.log(data)
-
-        const messageMQTT = new MQTTdata({
-            message: data.message,
-            title: data.title,
-            date: Date.now()
-        })
-
-        messageMQTT.save().then(() => {
-            console.log(messageMQTT)
-        }).catch((e) => {
-            console.log('Error!', e)
-        })
-    }
 })
 
 client.on('connect', () => {
