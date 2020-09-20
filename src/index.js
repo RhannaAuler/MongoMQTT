28f8435b8268ed500c6330f8eb957cad151c0ff6
marinastavares <marina@jungledevs.com>
2020-09-20 13:27:04 -0300

    change cors order

--- src/index.js
+++ src/index.js
@@ -10,13 +10,14 @@ const MQTTdata = require('./models/dataMQTT')
 const espRouter = require('./routers/esp')
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3001
 
 
-app.use(bodyParser.json()) // para entender quando mando json
 app.use(bodyParser.urlencoded({extended: false}))
-app.use(espRouter)
+app.use(bodyParser.json()) // para entender quando mando json
 app.use(cors())
+
+app.use(espRouter)
 
 
 app.listen(port)
